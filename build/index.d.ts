42b4fdf3fefabaec16915765c211c6683326377c
이웅희 <eunvanz@gmail.com>
2019-09-29 00:44:20 +0900

    build with webpack

--- build/index.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-interface IAutoheightSwipeableViewsProps {
-    activeIndex: number;
-    children: React.ReactNode;
-    onChangeIndex: (index: number) => void;
-    lazyLoading?: boolean;
-}
-declare const AutoheightSwipeableViews: ({ activeIndex, children, onChangeIndex, lazyLoading, }: IAutoheightSwipeableViewsProps) => JSX.Element;
-export default AutoheightSwipeableViews;
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  mode: 'production',
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+};
+
+module.exports = config;
